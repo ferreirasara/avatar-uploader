09f8052ac0abeb03b6257e8236a2bbd7b24997f2
Sara Ferreira <ferreirasara1501@gmail.com>
2023-04-18 14:09:26 -0300

    feat: tests for ImageContainer

--- src/components/__tests__/ImageContainer.test.tsx
+++ src/components/__tests__/ImageContainer.test.tsx
@@ -2,8 +2,13 @@ import renderer from 'react-test-renderer';
 import { ImageContainer } from '../ImageContainer';
 
 describe('ImageContainer', () => {
-  it('renders correctly', () => {
+  it('match snapshot (with image prop)', () => {
     const imageContainer = renderer.create(<ImageContainer image='https://dummyimage.com/largerectangle' />).toJSON();
+    expect(imageContainer).toMatchSnapshot();
+  });
+
+  it('match snapshot (without image prop)', () => {
+    const imageContainer = renderer.create(<ImageContainer />).toJSON();
     expect(imageContainer).toMatchSnapshot();
   });
 })
